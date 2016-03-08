9369aed747391ff733bb3b311e01c5897f7ef9d1
Maxime Mongeau <mongeaumax@gmail.com>
2016-03-08 16:44:07 -0500

    Forgot to init glfw

--- main.go
+++ main.go
@@ -3,10 +3,19 @@ package main
 import (
 	_ "github.com/go-gl/gl/v4.5-core/gl"
 	"github.com/go-gl/glfw/v3.1/glfw"
+	"runtime"
 )
 
+func init() {
+	runtime.LockOSThread()
+}
 func main() {
 	defer glfw.Terminate()
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+
 	window, err := glfw.CreateWindow(200, 200, "Testing", nil, nil)
 	if err != nil {
 		panic(err)
