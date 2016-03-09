715fc460c59c4a5c5fc9d4662a5cf15f01db92b3
Maxime Mongeau <mongeaumax@gmail.com>
2016-03-08 21:06:38 -0500

    Change OpenGL go binding to v4.1 My mac support up to 4.1 for now Add gl initialization

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ This repository is a place for me to learn how to use OpenGL in the Go
 programing language
 
 ## Dependencies
-I used go-gl/gl package to run OpenGL 4.5 and go-gl/glfw for window management
+I used go-gl/gl package to run OpenGL 4.1 and go-gl/glfw for window management
 (GLFW v3.1) with OpenGL.
 ```
-go get github.com/go-gl/gl/v4.5-core/gl
+go get github.com/go-gl/gl/v4.1-core/gl
 go get github.com/go-gl/glfw/v3.1/glfw
 ```
--- main.go
+++ main.go
@@ -1,28 +1,44 @@
 package main
 
 import (
-	_ "github.com/go-gl/gl/v4.5-core/gl"
+	"github.com/go-gl/gl/v4.1-core/gl"
 	"github.com/go-gl/glfw/v3.1/glfw"
 	"runtime"
 )
 
+const width = 800
+const height = 600
+
 func init() {
 	runtime.LockOSThread()
 }
+
 func main() {
 	defer glfw.Terminate()
-	err := glfw.Init()
-	if err != nil {
+
+	if err := glfw.Init(); err != nil {
 		panic(err)
 	}
 
-	window, err := glfw.CreateWindow(200, 200, "Testing", nil, nil)
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	window, err := glfw.CreateWindow(width, height, "Testing", nil, nil)
 	if err != nil {
 		panic(err)
 	}
-
 	window.MakeContextCurrent()
+
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+
 	for !window.ShouldClose() {
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
 		window.SwapBuffers()
 		glfw.PollEvents()
 	}
