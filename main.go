dd06e65a8afcd10294bc3cbfc3a0c48a935a9215
Maxime Mongeau <mongeaumax@gmail.com>
2016-03-11 22:00:19 -0500

    Create 2 triangles and reuse vertices with the EBO (element buffer object)

--- main.go
+++ main.go
@@ -39,24 +39,33 @@ func main() {
 	}
 
 	var vertices = []float32{
-		// First triangle
-		-0.5, -0.5, 0.0,
-		0.5, -0.5, 0.0,
-		0.0, 0.5, 0.0,
+		0.5, 0.5, 0.0, // Top right
+		0.5, -0.5, 0.0, // Bottom right
+		-0.5, -0.5, 0.0, // Bottom left
+		-0.5, 0.5, 0.0, // Top left
 	}
 
-	var vao uint32
+	var indices = []uint32{
+		0, 1, 3, // First triangle
+		1, 2, 3, // Second triangle
+	}
+
+	var vao, vbo, ebo uint32
 	gl.GenVertexArrays(1, &vao)
+	gl.GenBuffers(1, &vbo)
+	gl.GenBuffers(1, &ebo)
+
 	gl.BindVertexArray(vao)
 
-	var vbo uint32
-
-	gl.GenBuffers(1, &vbo)
 	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
 	gl.BufferData(gl.ARRAY_BUFFER, len(vertices)*4, gl.Ptr(vertices), gl.STATIC_DRAW)
 
+	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo)
+	gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, len(indices)*4, gl.Ptr(indices), gl.STATIC_DRAW)
+
 	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 3*4, gl.PtrOffset(0))
 	gl.EnableVertexAttribArray(0)
+
 	gl.BindVertexArray(0)
 
 	program, err := createProgram(vertexShaderSource, fragmentShaderSource)
@@ -70,7 +79,8 @@ func main() {
 
 		gl.UseProgram(program)
 		gl.BindVertexArray(vao)
-		gl.DrawArrays(gl.TRIANGLES, 0, 3)
+		// gl.DrawArrays(gl.TRIANGLES, 0, 6)
+		gl.DrawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, gl.PtrOffset(0))
 		gl.BindVertexArray(0)
 
 		window.SwapBuffers()
